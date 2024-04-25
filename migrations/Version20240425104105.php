3d84375a9312e7a81267e6746589bd22409a0100
Bartek <bartoszhumel@wp.pl>
2024-04-25 15:47:04 +0200

    update migration

--- migrations/Version20240425104105.php
+++ migrations/Version20240425104105.php
@@ -21,6 +21,7 @@ final class Version20240425104105 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE post (id INT NOT NULL, user_id INT DEFAULT NULL, username VARCHAR(255) DEFAULT NULL, title VARCHAR(255) DEFAULT NULL, body LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
